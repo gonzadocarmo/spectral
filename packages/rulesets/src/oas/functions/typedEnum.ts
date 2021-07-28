43c7ec24b71789c172465ddb29babe60eb4ec122
Jakub Rożek <jakub@rozek.tech>
2021-07-28 23:26:23 +0200

    perf(rulesets): avoid schema fn in typedEnum

--- packages/rulesets/src/oas/functions/typedEnum.ts
+++ packages/rulesets/src/oas/functions/typedEnum.ts
@@ -1,68 +1,70 @@
-import { schema } from '@stoplight/spectral-functions';
 import { oas2, oas3 } from '@stoplight/spectral-formats';
 import type { IFunction } from '@stoplight/spectral-core';
 import { isObject } from './utils/isObject';
+import { printValue } from '@stoplight/spectral-runtime';
+import type { JSONSchema4Type, JSONSchema6Type, JSONSchema7Type } from 'json-schema';
+
+function getType(input: unknown, checkForInteger: boolean): JSONSchema4Type | JSONSchema6Type | JSONSchema7Type {
+  switch (typeof input) {
+    case 'string':
+    case 'boolean':
+      return typeof input;
+    case 'number':
+      if (checkForInteger && Number.isInteger(input)) {
+        return 'integer';
+      }
+
+      return 'number';
+    case 'object':
+      if (input === null) {
+        return 'null';
+      }
+
+      return Array.isArray(input) ? 'array' : 'object';
+    default:
+      throw TypeError('Unknown input type');
+  }
+}
 
 export const typedEnum: IFunction = function (targetVal, opts, context) {
   if (!isObject(targetVal)) {
     return;
   }
 
-  if (targetVal.enum === null || targetVal.enum === void 0 || targetVal.type === null || targetVal.type === void 0) {
-    return;
-  }
-  // do not use rest spread operator here, as this causes the whole tslib gets injected despite proper target set...
-  // obviously, having tslib inlined makes the code size quite larger (around 4x after compression - 1.8K vs 7.4K).
   const { enum: enumValues } = targetVal;
-  const initialSchema = Object.assign({}, targetVal);
-  delete initialSchema.enum;
+  let { type } = targetVal;
 
-  if (!Array.isArray(enumValues)) {
+  if (!Array.isArray(enumValues) || (!Array.isArray(type) && typeof type !== 'string')) {
     return;
   }
 
   const { document } = context;
-  const isOAS3 = document.formats?.has(oas3) === true;
-  const isOAS2 = document.formats?.has(oas2) === true;
+  const checkForInteger = Array.isArray(type) ? type.includes('integer') : type === 'integer';
 
-  let innerSchema;
-  if ((isOAS3 && targetVal.nullable === true) || (isOAS2 && targetVal['x-nullable'] === true)) {
-    const type = Array.isArray(initialSchema.type)
-      ? [...(initialSchema.type as unknown[])]
-      : initialSchema.type !== void 0
-      ? [initialSchema.type]
-      : [];
-    if (!type.includes('null')) {
-      type.push('null');
-    }
-
-    innerSchema = { type, enum: initialSchema.enum };
-  } else {
-    innerSchema = { type: initialSchema.type, enum: initialSchema.enum };
+  if (
+    (document.formats?.has(oas3) === true && targetVal.nullable === true) ||
+    (document.formats?.has(oas2) === true && targetVal['x-nullable'] === true)
+  ) {
+    (Array.isArray(type) ? type : (type = [type])).push('null');
   }
 
-  const schemaObject = { schema: innerSchema };
+  let incorrectValues: number[] | undefined;
 
-  const incorrectValues: Array<{ index: number; val: unknown }> = [];
+  (enumValues as unknown[]).forEach((value, i) => {
+    const valueType = getType(value, checkForInteger);
 
-  (enumValues as unknown[]).forEach((val, index) => {
-    const res = schema(val, schemaObject, context);
-
-    if (Array.isArray(res) && res.length !== 0) {
-      incorrectValues.push({ index, val });
+    if (Array.isArray(type) ? type.includes(valueType) : valueType === type) {
+      return;
     }
+
+    incorrectValues ??= [];
+    incorrectValues.push(i);
   });
 
-  if (incorrectValues.length === 0) {
-    return;
-  }
-
-  const { type } = initialSchema;
-
-  return incorrectValues.map(bad => {
+  return incorrectValues?.map(i => {
     return {
-      message: `Enum value \`${String(bad.val)}\` must be "${String(type)}".`,
-      path: [...context.path, 'enum', bad.index],
+      message: `Enum value ${printValue(enumValues[i])} must be "${String(type)}".`,
+      path: [...context.path, 'enum', i],
     };
   });
 };
